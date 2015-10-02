45c574016ec0704158bfa74b7c5cee04e0552647
Kevin van der Vlist <kvdvlist@sogyo.nl>
2015-10-02 08:56:45 +0200

    Initial cache is completely offline

--- frontend/app/scripts/factory/domain_cache.js
+++ frontend/app/scripts/factory/domain_cache.js
@@ -3,10 +3,17 @@
 angular.module('frontendApp')
   .factory('DomainCache', ['falcor', function (falcor) {
     var $ref = falcor.Model.ref;
+
+    /** Complete offline cache **/
     return {
       cache: {
         // customer service
         customer: {
+          "size": 2,
+          "all": [
+            $ref("customer['0f22b52a-a530-44b9-ac10-54dd35c6b49f']"),
+            $ref("customer['9a22b52a-a531-44b9-ff10-64dd35c6b49f']")
+          ],
           "0f22b52a-a530-44b9-ac10-54dd35c6b49f": {
             firstName: "First",
             lastName: "Last",
@@ -16,9 +23,9 @@ angular.module('frontendApp')
             ]
           },
           "9a22b52a-a531-44b9-ff10-64dd35c6b49f": {
-            name: "foo",
-            otherName: "bar",
-            address: "foo@bar.baz",
+            firstName: "foo",
+            lastName: "bar",
+            email: "foo@bar.baz",
             order: []
           }
         },
@@ -53,6 +60,11 @@ angular.module('frontendApp')
                 "product": $ref("product['ea150aec-fe33-4264-9097-0f2608b56485']")
               }
             ]
+          }
+        },
+        "discount": {
+          "285e56ff-9499-46bd-90d9-fa23e717f376": {
+            "percentage": 45
           }
         }
       }
