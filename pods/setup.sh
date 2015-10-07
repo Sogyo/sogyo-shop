44cfc2ca175997d959f77505d576506bc0c0b86f
Kevin van der Vlist <kvdvlist@sogyo.nl>
2015-10-07 17:36:23 +0200

    pod definition updates.

--- pods/setup.sh
+++ pods/setup.sh
@@ -5,13 +5,11 @@ read
 kubectl -s 10.199.0.30:8080 create -f customer-java-rc.yaml
 kubectl -s 10.199.0.30:8080 create -f customer-java-service.yaml
 read
-kubectl -s 10.199.0.30:8080 create -f discount-aspnet-rc.yaml
-kubectl -s 10.199.0.30:8080 create -f discount-aspnet-service.yaml
-read
 kubectl -s 10.199.0.30:8080 create -f falcor-domain-router-rc.yaml
 kubectl -s 10.199.0.30:8080 create -f falcor-domain-router-service.yaml
 read
 kubectl -s 10.199.0.30:8080 create -f frontend-angular-rc.yaml
 kubectl -s 10.199.0.30:8080 create -f frontend-angular-service.yaml
-
-
+read
+kubectl -s 10.199.0.30:8080 create -f discount-aspnet-rc.yaml
+kubectl -s 10.199.0.30:8080 create -f discount-aspnet-service.yaml
