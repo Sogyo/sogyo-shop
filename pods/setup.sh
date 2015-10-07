12acbd051eae2b76715c5abcbc4dae06db4fe5bb
Kevin van der Vlist <kvdvlist@sogyo.nl>
2015-10-07 12:31:17 +0200

    Use go image by default, since it is the smallest container.

--- pods/setup.sh
+++ pods/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+kubectl -s 10.199.0.30:8080 create -f discount-go-rc.yaml
+kubectl -s 10.199.0.30:8080 create -f discount-go-service.yaml
+read
 kubectl -s 10.199.0.30:8080 create -f customer-java-rc.yaml
 kubectl -s 10.199.0.30:8080 create -f customer-java-service.yaml
 read
 kubectl -s 10.199.0.30:8080 create -f discount-aspnet-rc.yaml
 kubectl -s 10.199.0.30:8080 create -f discount-aspnet-service.yaml
-read
-kubectl -s 10.199.0.30:8080 create -f discount-go-rc.yaml
-kubectl -s 10.199.0.30:8080 create -f discount-go-service.yaml
 read
 kubectl -s 10.199.0.30:8080 create -f falcor-domain-router-rc.yaml
 kubectl -s 10.199.0.30:8080 create -f falcor-domain-router-service.yaml
